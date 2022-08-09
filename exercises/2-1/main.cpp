cc920b82aa17503115fa41f19c1745e8663c4d65
chendianbuji <chendianbuji@gmail.com>
2022-08-09 12:32:05 +0800

    exercises 2

--- exercises/2-1/main.cpp
+++ exercises/2-1/main.cpp
@@ -7,31 +7,53 @@ enum class Operation {
   Divide
 };
 
-int main() {
-  Operation op{Operation::Add};
-  switch(op) {
-  case Operation::Add:
-    {
-      std::cout << "add" << std::endl;
-    }
-    break;
-  case Operation::Subtract:
-    {
-      std::cout << "subtract" << std::endl;
-    }
-    break;
-  case Operation::Multiply:
-    {
-      std::cout << "multiply" << std::endl;
-    }
-    break;
-  case Operation::Divide:
-    {
-      std::cout << "divide" << std::endl;
-    }
-    break;
-  default:
-    break;
+struct Calculator {
+
+  Operation operation;
+  
+  Calculator(Operation op) {
+    operation = op;
   }
+
+  int calculate(int a, int b) {
+    int result;
+    switch(operation) {
+    case Operation::Add:
+      {
+        result = a + b;
+      }
+      break;
+    case Operation::Subtract:
+      {
+        result = a - b;
+      }
+      break;
+    case Operation::Multiply:
+      {
+        result = a * b;
+      }
+      break;
+    case Operation::Divide:
+      {
+        result = a / b;
+      }
+      break;
+    default:
+      break;
+    }
+    return result;
+  }
+};
+
+int main() {
+  Calculator cal1 = {Operation::Add};
+  Calculator cal2{Operation::Subtract};
+  Calculator cal3(Operation::Multiply);
+  Calculator cal4 = {Operation::Divide};;
+
+  std::cout << cal1.calculate(1, 2) << std::endl;
+  std::cout << cal2.calculate(1, 2) << std::endl;
+  std::cout << cal3.calculate(1, 2) << std::endl;
+  std::cout << cal4.calculate(6, 2) << std::endl;
   return 0;
 }
